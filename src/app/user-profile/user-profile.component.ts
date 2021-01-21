147f06093f8f7b2aaa6f42b1cafd2d4386e1552a
Anawat Khamdee <aon@odds.team>
2021-01-21 20:00:05 +0700

    Example

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Global } from '../global/global';
 import { DropdownDepartmentModel, DropdownDepartmentModelList, RequestRegister, ResponseModel, UserProfileModel } from '../Models/UserModel.model';
@@ -58,7 +58,7 @@ export class UserProfileComponent implements OnInit {
 
   public initForm(): void {
     this.formGroup = new FormGroup({
-      isUsername: new FormControl(''),
+      isUsername: new FormControl('', [Validators.required]),
       isPassword: new FormControl(''),
       isRePassword: new FormControl(''),
       isName: new FormControl(''),
